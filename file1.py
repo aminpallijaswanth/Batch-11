70cf276d6bdd77cef002f547b76060d35b9b1247
aminpallijaswanth <162982690+aminpallijaswanth@users.noreply.github.com>
2024-03-11 16:10:52 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,39 @@
+'''
+Bargained characters
+1. Controls and data structures -> ABC languages
+2. Strings and its format -> from C
+3. Regular expressions -> Pearl
+4. Exception handling -> Module 3
+5. Magic methods from -> fortron
+'''
+
+
+#print("Hello World")
+
+import this
+print(this)
+
+#from file_1. import a, b
+#print(a)
+#print(b)
+
+import antigravity
+
+
+# ! ways to define variables
+
+a = 8
+b = 9
+print(a,b)
+
+a=b=7
+print(a,b)
+
+a=8,7,9.89,"hello"
+print(a)
+print(type(a))
+
+a, b= 78
+print(a, b)
+
+              
