f25c57453ebd05322dcf1b8de7e9bc8e1ff4b8d5
aminpallijaswanth <162982690+aminpallijaswanth@users.noreply.github.com>
2024-03-11 18:26:12 +0530

    Add files via upload

--- file1.py
+++ file1.py
@@ -1,39 +1,54 @@
+#print("hello")
+print("hello world")
 '''
-Bargained characters
-1. Controls and data structures -> ABC languages
-2. Strings and its format -> from C
-3. Regular expressions -> Pearl
-4. Exception handling -> Module 3
-5. Magic methods from -> fortron
+1.commenting is used for making documentation in a phyton file
+2.when a line is commented, that line will be not considered as a part of code
+3.whwn program is excueted, commented line will not be in part of exucution
 '''
-
-
-#print("Hello World")
+print("hello world")
+print("hello world")
+print("hello world")
 
 import this
 print(this)
 
-#from file_1. import a, b
-#print(a)
-#print(b)
-
 import antigravity
+'''
+------->Bargained charecters
+1.Contrtols and data dtructures ---> ABC language
+2.Stings and its formates ---> from C
+3.Regular expressions ---> Peral
+4.Exception handling ---> Module 3
+5.Magic methods ---> forton
+'''
 
-
-# ! ways to define variables
-
-a = 8
-b = 9
-print(a,b)
-
-a=b=7
-print(a,b)
-
-a=8,7,9.89,"hello"
-print(a)
-print(type(a))
-
-a, b= 78
-print(a, b)
-
-              
+'''
+------>what kind of language of phyton
+1. Object oriented program language
+2. Dynamically typed language
+3. Scripting language
+4. Interepted language
+5. Compiled language
+6.functional language
+'''
+'''
+-------->Flavours of language
+> Cpython - written in C, Most common implementation of python
+> Jython - Java implementation of python
+> Ironpython - Implementation of the python progarmming language which is highly integrated with .NET.
+> Bryton- Browser python ,runs in browser
+> Rubypython - Bridge b/w pythonb and Ruby Interporters
+> Micropython - Runs in micro controller
+'''
+'''
+----> variables
+>variable is a name which is used to identify the value of variable
+>which is stored in a specific memory location
+# Rules to define variables
+1. Variable have to start with only alphabets, under scores
+2. The char following the 1st char can be alphabets, numbers, under scores
+3. No Space are allowed
+4. No special charecteres like !@#$%^&*()+
+5. case sensitive
+6. keyboards are not allowed as variables
+# ways to define variables
